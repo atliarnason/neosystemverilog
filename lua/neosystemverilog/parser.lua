a31d36f652171c7b404ef506c542b4c8533574c4
atlihrafnarnason <atli@gormur.com>
2025-12-30 20:58:25 +0100

    (parser): parser general test

--- lua/neosystemverilog/parser.lua
+++ lua/neosystemverilog/parser.lua
@@ -6,8 +6,28 @@
 local M = {}
 
 function M.check_syntax()
-  -- TODO: Implement Verilator/Iverilog syntax checking
-  require('neosystemverilog.utils').info('Syntax checking not yet implemented')
+  -- TODO: elaborate this function 
+  local config = require('neosystemverilog.config')
+  local utils = require('neosystemverilog.utils')
+  
+  local file = vim.fn.expand('%:p')
+  
+  if config.get().linter.tool == 'verilator' then
+    local verilator = config.get().linter.verilator_path
+    local cmd = string.format('%s --lint-only %s 2>&1', verilator, file)
+    
+    local output, exit_code = utils.execute_command(cmd)
+    
+    if exit_code == 0 then
+      utils.info('No syntax errors found')
+    else
+      utils.warn('Syntax errors detected')
+      -- You can populate quickfix here later
+      for _, line in ipairs(output) do
+        print(line)
+      end
+    end
+  end 
 end
 
 function M.update_index()
